07c39b5c97c52fc8ccbd64f5a04b2f00bcf425a1
Dmitriy <argizol@mail.ru>
2022-09-15 00:04:19 +0300

    Рефакторинг печати результата поиска. Убран ненужный цикл

--- GroupProjectHW7-9/GroupProjectHW7-9/FindCommands.py
+++ GroupProjectHW7-9/GroupProjectHW7-9/FindCommands.py
@@ -3,10 +3,11 @@ from Contacts import data
 
 def find_contact(user_data):
     global data
+    number = 0
     filtered_list = []
     for name, phone in data.items():
         if (user_data in name) or (user_data in phone):
             filtered_list.append(f'{name},{phone}')
-    for idx, el in enumerate(filtered_list):
-        print(idx + 1, el)
+            number += 1
+            print(f'{number} {name}, {phone}')
     return filtered_list
