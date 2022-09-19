967aab7adbed914647de8be146991e11e94acc80
Dmitriy <argizol@mail.ru>
2022-09-19 10:24:49 +0300

    Чистка кода

--- GroupProjectHW7-9/GroupProjectHW7-9/FindCommands.py
+++ GroupProjectHW7-9/GroupProjectHW7-9/FindCommands.py
@@ -12,18 +12,3 @@ def find_contact(user_data):
             print(f'{number} {name}, {phone}')
     return filtered_list
 
-def find_contact_bot():
-    global data
-    filtered_list = []
-    count = 0
-    for name, phone in data.items():
-        if (user_data in name) or (user_data in phone):
-            filtered_list.append(f'{name},{phone}')
-    keyboard = types.InlineKeyboardMarkup()
-    for i in filtered_list:
-            count+=1
-            button = types.InlineKeyboardButton(text= filtered_list[i], callback_data= count)
-            keyboard.add(button)
-
-    #question = f'Фамилия и имя: {name} {surname}, телефон {str(phonenumber)}. Все верно?'
-    #bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
